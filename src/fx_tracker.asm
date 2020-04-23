670557a62cb4f302e027d2ee27839f50dfcbc526
Kieran Connell <kieran@kconnell.co.uk>
2020-04-23 17:46:21 +0100

    Events handler and preload jump table.

--- src/fx_tracker.asm
+++ src/fx_tracker.asm
@@ -49,6 +49,17 @@ MACRO EVENTS_GET_BYTE
 }
 ENDMACRO
 
+MACRO EVENTS_PEEK_BYTE
+{
+    lda events_load_byte+1
+    sta events_ptr
+    lda events_load_byte+2
+    sta events_ptr+1
+    ldy #1
+    lda (events_ptr), Y
+}
+ENDMACRO
+
 .events_init
 {
     lda #LO(event_data-1)
@@ -63,7 +74,21 @@ ENDMACRO
     sta events_delay
     EVENTS_GET_BYTE
     sta events_delay+1
-    rts
+
+    \\ Call preload fn for next event.
+    EVENTS_PEEK_BYTE
+
+    tay
+    and #&f0:
+    lsr a:lsr a:tax
+    lda event_fn_table+2, X
+    sta jmp_to_preload+1
+    lda event_fn_table+3, X
+    sta jmp_to_preload+2
+    tya
+    and #&0f
+    .jmp_to_preload
+    jmp &FFFF
 }
 
 .events_get_byte
@@ -111,8 +136,52 @@ ENDMACRO
 ; A = event number
 .events_handler
 {
+    tay
+    and #&f0:
+    lsr a:lsr a:tax
+    lda event_fn_table+0, X
+    sta jmp_to_handler+1
+    lda event_fn_table+1, X
+    sta jmp_to_handler+2
+    tya
+    and #&0f
+    .jmp_to_handler
+    jmp &FFFF
+}
+
+.handle_set_colour
+{
     eor #7
-    jsr set_fg_colour
+    jmp set_fg_colour
+}
+
+.event_fn_table
+{
+\\ Event handler and preload fn per event type &xy
+    EQUW handle_set_colour,     do_nothing  ; &0y
+    EQUW handle_image,          do_nothing  ; &1y
+    EQUW handle_anim,           do_nothing  ; &2y
+    EQUW handle_set_colour,     do_nothing  ; &3y
+    EQUW handle_special_fx,     do_nothing  ; &4y
+    EQUW do_nothing,            do_nothing  ; &5y
+    EQUW do_nothing,            do_nothing  ; &6y
+    EQUW do_nothing,            do_nothing  ; &7y
+    EQUW do_nothing,            do_nothing  ; &8y
+    EQUW do_nothing,            do_nothing  ; &9y
+    EQUW do_nothing,            do_nothing  ; &Ay
+    EQUW do_nothing,            do_nothing  ; &By
+    EQUW do_nothing,            do_nothing  ; &Cy
+    EQUW do_nothing,            do_nothing  ; &Dy
+    EQUW do_nothing,            do_nothing  ; &Ey
+    EQUW do_nothing,            do_nothing  ; &Fy
+}
+
+.handle_ctrl_code
+.handle_image
+.handle_anim
+.handle_special_fx
+.do_nothing
+{
     rts
 }
 

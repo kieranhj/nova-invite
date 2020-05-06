f5038a3817875a641aea20fd16645fa8bdbd7137
Kieran Connell <kieran@kconnell.co.uk>
2020-05-06 16:55:23 +0100

    New music drop.

--- make-music.bat
+++ make-music.bat
@@ -1,6 +1,6 @@
 @echo off
 mkdir build
 echo Building MUSIC...
-bin\SongToYM.exe --psg 1 data\music\Acid_demo_07.aks build\acid_demo.ym
+bin\SongToYM.exe --psg 1 data\music\Acid_demo_08.aks build\acid_demo.ym
 python bin\ym2sn.py --white build/acid_demo.ym -o build/acid_demo.vgm
 python bin\vgmpacker.py build/acid_demo.vgm -o build/acid_demo.vgc

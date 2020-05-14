27d9cd759b09d3041cfa6106f2b13467f2d0038e
Kieran Connell <kieran@kconnell.co.uk>
2020-05-14 22:22:25 +0100

    Another font drop.
    Plus additional text.

--- src/text_blocks.asm
+++ src/text_blocks.asm
@@ -6,9 +6,9 @@
 .text_block_table
 {
     EQUW text_block_test
+    EQUW text_block_credits
     EQUW text_block_greetz
-    EQUW 0
-    EQUW 0
+    EQUW text_block_greetz2
     EQUW 0
     EQUW 0
     EQUW 0
@@ -42,7 +42,7 @@ EQUS 31,2,5,  "KIERAN"
 EQUS 31,4,10, "RHINO"
 EQUS 31,6,15, "SPINY"
 EQUS 31,8,20, "HENLEY"
-EQUS 31,10,25,"0XCODE"
+EQUS 31,10,25,"0XC0DE"
 EQUS 0
 
 .text_block_greetz
@@ -52,15 +52,17 @@ EQUS 31,0,0, "GREETZ"
 EQUS 31,0,5, "ATE-BIT"
 EQUS 31,0,10,"CRTC"
 EQUS 31,0,15,"DESIRE"
-EQUS 31,0,20,"HOOY-PROGRAM"
-EQUS 31,0,25,"INVERSE PHASE"
+EQUS 31,0,20,"HOOY"
+EQUS 31,0,25,"-PROGRAM"
+EQUS 0
 
 .text_block_greetz2
 EQUS 12; cls
 ;            "|--------|"
-EQUS 31,0,0,"LOGICOMA"
-EQUS 31,0,5,"POLARITY"
+EQUS 31,0,0, "LOGICOMA"
+EQUS 31,0,5, "POLARITY"
 EQUS 31,0,10,"RIFT"
 EQUS 31,0,15,"SLIPSTREAM"
 EQUS 31,0,20,"YM ROCKERZ"
+EQUS 31,0,25,"INV*PHASE"
 EQUS 0

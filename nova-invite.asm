f6f2fd8d568130cff39c41e6133c79c567c5b177
Kieran Connell <kieran@kconnell.co.uk>
2020-05-20 00:22:06 +0100

    Move asset includes into main asm file.

--- nova-invite.asm
+++ nova-invite.asm
@@ -929,7 +929,38 @@ CLEAR &8000, &C000
 ORG &8000
 GUARD &C000
 .bank0_start
-include "src/image_assets.asm"
+
+.exo_image_big_n
+INCBIN "build/image_n.exo"
+.exo_image_big_o
+INCBIN "build/image_o.exo"
+.exo_image_big_v
+INCBIN "build/image_v.exo"
+.exo_image_big_a
+INCBIN "build/image_a.exo"
+.exo_image_nova
+INCBIN "build/image_nova.exo"
+.exo_image_nova_2020
+INCBIN "build/image_nova2020.exo"
+.exo_image_bs_logo2
+INCBIN "build/image_bs_logo2.exo"
+.exo_image_tmt_logo
+INCBIN "build/image_tmt_logo.exo"
+.exo_slide_beach
+INCBIN "build/slide_beach.exo"
+.exo_slide_chips
+INCBIN "build/slide_chips.exo"
+.exo_slide_patarty
+INCBIN "build/slide_patarty.exo"
+.exo_image_bbc_owl
+INCBIN "build/image_bbc_owl.exo"
+.exo_slide_floppy
+INCBIN "build/slide_floppies.exo"
+.exo_slide_djsets
+INCBIN "build/slide_djset.exo"
+;.exo_slide_firealarm
+;INCBIN "build/slide_firealarm.exo"
+
 .bank0_end
 
 SAVE "build/BANK0", bank0_start, bank0_end, bank0_start

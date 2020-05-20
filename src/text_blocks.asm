0821f2e376a1f5a428fb8af925d649d20790dbf5
Kieran Connell <kieran@kconnell.co.uk>
2020-05-20 13:41:55 +0100

    Added new text blocks.
    Tidied up spacing.

--- src/text_blocks.asm
+++ src/text_blocks.asm
@@ -17,6 +17,10 @@
     EQUW text_slide_bitshifters     ; c809
     EQUW text_slide_dates           ; c80A
     EQUW text_slide_firealarm       ; c80B
+    EQUW text_block_greetz3         ; c80C
+    EQUW text_block_just_dates      ; c80D
+    EQUW text_block_see_you         ; c80E
+    EQUW text_block_compos          ; c80F
 }
 
 .text_block_test
@@ -33,34 +37,43 @@ EQUS 0
 .text_block_credits
 EQUS 12; cls
 ;             "|--------|"
-EQUS 31,0,0,  "CREDITS"
-EQUS 31,2,5,  "KIERAN"
-EQUS 31,4,10, "RHINO"
-EQUS 31,6,15, "SPINY"
-EQUS 31,8,20, "HENLEY"
-EQUS 31,10,25,"0XC0DE"
+EQUS 31,0,0,  "CREDITS*"
+EQUS 31,2,6,  "KIERAN"
+EQUS 31,4,11, "RHINO"
+EQUS 31,6,16, "SPINY"
+EQUS 31,8,21, "HENLEY"
+EQUS 31,10,26,"0XC0DE"
 EQUS 0
 
 .text_block_greetz
 EQUS 12; cls
 ;            "|--------|"
-EQUS 31,0,0, "GREETZ"
-EQUS 31,0,5, "ATE-BIT"
-EQUS 31,0,10,"CRTC"
-EQUS 31,0,15,"DESIRE"
-EQUS 31,0,20,"HOOY"
-EQUS 31,0,25,"-PROGRAM"
+EQUS 31,0,0, "GREETZ*"
+EQUS 31,2,6, "ATE-BIT"
+EQUS 31,3,11,"CRTC"
+EQUS 31,4,16,"DESIRE"
+EQUS 31,5,21,"HOOY"
+EQUS 31,6,26,"-PROGRAM"
 EQUS 0
 
 .text_block_greetz2
 EQUS 12; cls
 ;            "|--------|"
-EQUS 31,0,0, "LOGICOMA"
-EQUS 31,0,5, "POLARITY"
-EQUS 31,0,10,"RIFT"
-EQUS 31,0,15,"SLIPSTREAM"
-EQUS 31,0,20,"YM ROCKERZ"
-EQUS 31,0,25,"INV*PHASE"
+EQUS 31,0,0, "GREETZ*"
+EQUS 31,2,6, "LOGICOMA"
+EQUS 31,3,11,"POLARITY"
+EQUS 31,4,16,"RIFT"
+EQUS 31,4,21,"SLIPSTREAM"
+EQUS 31,3,26,"YM ROCKERZ"
+EQUS 0
+
+.text_block_greetz3
+EQUS 12; cls
+;            "|--------|"
+EQUS 31,0,0, "GREETZ*"
+EQUS 31,2,6, "BUDBRAIN"
+EQUS 31,3,11,"INVERSE"
+EQUS 31,8,16,"PHASE"
 EQUS 0
 
 .text_slide_beach
@@ -102,19 +115,43 @@ EQUS 0
 EQUS 22, &07            ; bitshifters flux
 ;            "|--------|"
 EQUS 31,0,0, "BITSHIFTERS"
-EQUS 31,0,27,"BITSHIFTERS"
+EQUS 31,0,28,"BITSHIFTERS"
 EQUS 0
 
 .text_slide_dates
-EQUS 22, &04            ; NOVA
+EQUS 22, &15            ; NOVA URL
 ;            "|--------|"
-EQUS 31,15,7,"ONLINE"
-EQUS 31,4,24,"19-21 JUNE"
+EQUS 31,4,22,"19-21 JUNE"
 EQUS 0
 
 .text_slide_firealarm
 EQUS 22, &11            ; fire alarm
 ;            "|--------|"
 EQUS 31,3,0, "FIRE ALARM"
-EQUS 31,3,27,"[OPTIONAL]"
+EQUS 31,3,27,"(OPTIONAL)"
+EQUS 0
+
+.text_block_just_dates
+EQUS 12
+;            "|--------|"
+EQUS 31,4,14,"19-21 JUNE"
+EQUS 0
+
+.text_block_see_you
+EQUS 12
+;             "|--------|"
+EQUS 31,7,7,  "SEE YOU"
+EQUS 31,8,12, "ONLINE!"
+EQUS 31,4,20, "19-21 JUNE"
+EQUS 0
+
+.text_block_compos
+EQUS 12
+;             "|--------|"
+EQUS 31,3,1,  "COMPOS!"
+EQUS 31,4,6,  "COMPOS!"
+EQUS 31,5,11, "COMPOS!"
+EQUS 31,6,16, "COMPOS!"
+EQUS 31,7,21, "COMPOS!"
+EQUS 31,8,26, "COMPOS!"
 EQUS 0
